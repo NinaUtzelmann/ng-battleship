525d8380148a44b55831a32a13fa5667b754c03c
Nina Utzelmann <nina-marie.utzelmann@hotmail.de>
2017-01-18 17:17:24 +0100

    first push with angular

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+    import { Component } from '@angular/core';
+
+    @Component({
+      selector: 'my-app',
+      template:`{{}}`
+	  
+	})
+    export class AppComponent {
+		items = [1, 2 ,3 , 4, 5, 6, 7, 8, 9, 10],
+    }
+	
+
+	
